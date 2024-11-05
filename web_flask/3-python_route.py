008389a0e5dbee96e02758af4208811baeccf22b
Dtbissy <isaactheedwards@gmail.com>
2024-11-05 16:02:13 -0600

    $ done

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,6 +26,9 @@ def C(text):
 def python(text="is cool"):
     return "Python {}".format(text.replace("_", " "))
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
 
 if __name__ == '__main__' :
     app.run(host='0.0.0.0', port=5000)
