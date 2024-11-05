7160d175d4fde8fb28d8073a28672459f39130d7
Dtbissy <isaactheedwards@gmail.com>
2024-11-05 15:57:34 -0600

    QUick and easy

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -64,6 +64,10 @@ class DBStorage:
         if obj is not None:
             self.__session.delete(obj)
 
+    def close(self):
+        """Close the database session."""
+        self.__session.close()
+
     def reload(self):
         """reloads data from the database"""
         Base.metadata.create_all(self.__engine)
